61b604566c732133e721be9787bd2fc61f1419a5
Mediavilla Jérémy <jeremy.mediavilla@epitech.eu>
2015-05-26 13:45:36 +0200

    parser xml màj

--- parserXML/main.c
+++ parserXML/main.c
@@ -5,7 +5,7 @@
 ** Login   <mediav_j@epitech.net>
 ** 
 ** Started on  Thu May 21 17:08:57 2015 Jérémy Mediavilla
-** Last update Fri May 22 16:21:51 2015 Jérémy Mediavilla
+** Last update Tue May 26 13:21:10 2015 Jérémy Mediavilla
 */
 
 #include "parserXml.h"
@@ -53,6 +53,21 @@ int		getTagPos(t_tag *tab, char *tagName)
   return (-1);
 }
 
+xmlChar		*getValueFromTag(xmlNodePtr cur, char *tag)
+{
+  xmlNodePtr	tmp;
+
+  tmp = cur->xmlChildrenNode;
+  while (tmp != NULL)
+    {
+      printf("%s\n", tmp->name);
+      if (xmlStrcmp(tmp->name, (const xmlChar *) tag) == 0)
+	return (xmlNodeGetContent(tmp));
+      tmp = tmp->next;
+    }
+  return (NULL);
+}
+
 t_tag		*isMultipleNode(xmlNodePtr cur)
 {
   t_tag		*tags;
@@ -61,12 +76,10 @@ t_tag		*isMultipleNode(xmlNodePtr cur)
   int		mult;
   int		j;
 
-  /* Vérifier si la balise est valide */
+  /* Vérifier si la balise est valide - ou pas */
   tags = malloc(42 * sizeof(t_tag));
   for (i = 0; i < 42; i++)
-    {
-      tags[i].name = NULL;
-    }
+    tags[i].name = NULL;
   i = 0;
   mult = 0;
   cur = cur->xmlChildrenNode;
@@ -108,10 +121,140 @@ void		multipleNodeError(t_tag *tags)
   for (i = 0; tags[i].name != NULL; i++)
     if (tags[i].nb > 1)
       {
-	fprintf(stderr, "Error: multiple déclaration of <%s> :\n", tags[i].name);
+	fprintf(stderr, "Error: multiple déclaration of \"<%s>\" :\n", tags[i].name);
 	for (j = 0; tags[i].lines[j] > 0; j++)
 	  fprintf(stderr, "\tdeclared line %i\n", tags[i].lines[j]);
       }
+}
+
+void		getParameters(xmlNodePtr cur)
+{
+  t_tag		*tmp;
+
+  if ((tmp = isMultipleNode(cur)) != NULL)
+    {
+      multipleNodeError(tmp);
+      free(tmp);
+      /* return */
+    }
+  printf("--> param\n");
+}
+
+void		getTextures(xmlNodePtr cur)
+{
+  t_tag		*error;
+
+  cur = cur->xmlChildrenNode;
+  while (cur != NULL)
+    {
+      if (xmlStrcmp(cur->name, (const xmlChar *)"texture"))
+	{
+	  fprintf(stderr, "Error : unknown tag \"<%s>\" declared line %i\n", cur->name, cur->line);
+	  /* return */
+	}
+      if ((error = isMultipleNode(cur)) != NULL)
+	{
+	  multipleNodeError(error);
+	  free(error);
+	  /* return */
+	}
+      cur = cur->next;
+    }
+  printf("--> texture\n");
+}
+
+void		getSounds(xmlNodePtr cur)
+{
+  t_tag		*error;
+
+  cur = cur->xmlChildrenNode;
+  while (cur != NULL)
+    {
+      if (xmlStrcmp(cur->name, (const xmlChar *)"sound"))
+	{
+	  fprintf(stderr, "Error : unknown tag \"<%s>\" declared line %i\n", cur->name, cur->line);
+	  /* return */
+	}
+      if ((error = isMultipleNode(cur)) != NULL)
+	{
+	  multipleNodeError(error);
+	  free(error);
+	  /* return */
+	}
+      cur = cur->next;
+    }
+  printf("--> sound\n");
+}
+
+void		getCommon(xmlNodePtr cur)
+{
+  t_tag		*tmp;
+
+  if ((tmp = isMultipleNode(cur)) != NULL)
+    {
+      multipleNodeError(tmp);
+      free(tmp);
+      /* return */
+    }
+  printf("--> common\n");
+}
+
+void		getEntities(xmlNodePtr cur)
+{
+  xmlNodePtr	child;
+  t_tag		*tmp;
+
+  cur = cur->xmlChildrenNode;
+  while (cur != NULL)
+    {
+      if (xmlStrcmp(cur->name, (const xmlChar *)"entity"))
+	{
+	  fprintf(stderr, "Error : unknown tag \"<%s>\" declared line %i\n", cur->name, cur->line);
+	  /* return */
+	}
+      if ((tmp = isMultipleNode(cur)) != NULL)
+	{
+	  multipleNodeError(tmp);
+	  free(tmp);
+	  /* return */
+	}
+      child = cur->xmlChildrenNode;
+      while (child != NULL)
+	{
+	  if (xmlStrcmp(child->name, (const xmlChar *)"attribut") == 0)
+	    printf("***** %s\n", child->name);
+	  printf("\tchild : %s\n", child->name);
+	  child = child->next;
+	}
+      cur = cur->next;
+    }
+  printf("--> entities\n");
+}
+
+int		parseTag(char *tag, xmlNodePtr cur)
+{
+  void		(*whichTag[5])();
+  char		*tagTab[5];
+  int		i;
+
+  whichTag[0] = &getParameters;
+  whichTag[1] = &getTextures;
+  whichTag[2] = &getSounds;
+  whichTag[3] = &getCommon;
+  whichTag[4] = &getEntities;
+  tagTab[0] = PARAMETERS_TAG;
+  tagTab[1] = TEXTURE_TAG;
+  tagTab[2] = SOUND_TAG;
+  tagTab[3] = COMMON_TAG;
+  tagTab[4] = ENTITY_TAG;
+  for (i = 0; i != 5; i++)
+    if (strcmp(tag, tagTab[i]) == 0)
+      {
+	(*whichTag[i])(cur);
+	return (0);
+      }
+  fprintf(stderr, "Error : unknown tag \"%s\" at line %i\n", tag, cur->line);
+  return (-1);
 }
 
 int		main(int ac, char **av)
@@ -119,7 +262,6 @@ int		main(int ac, char **av)
   xmlDocPtr	doc;
   xmlNodePtr	cur;
   t_tag		*tmp;
-  /* xmlNodePtr	tmp; */
 
   xmlKeepBlanksDefault(0);
   if (ac != 2)
@@ -129,27 +271,26 @@ int		main(int ac, char **av)
     }
   if (loadFile(&doc, av[1]) == -1 || docGetRoot(&cur, doc) == -1 || checkRoot(cur, doc) == -1)
     return (EXIT_FAILURE);
+  if ((tmp = isMultipleNode(cur)) != NULL)
+    {
+      multipleNodeError(tmp);
+      free(tmp);
+      xmlFree(cur);
+      xmlFreeDoc(doc);
+      return (EXIT_FAILURE);
+    }
   cur = cur->xmlChildrenNode;
   while (cur != NULL)
     {
-      if (xmlStrcmp(cur->name, (const xmlChar *)"parameters") == 0)
-      	{
-	  if ((tmp = isMultipleNode(cur)) != NULL)
-	    {
-	      multipleNodeError(tmp);
-	      free(tmp);
-	    }
-      	  /* tmp = cur->xmlChildrenNode; */
-      	  /* while (tmp != NULL) */
-      	  /*   { */
-      	  /*     printf("\t[%s]\n", xmlNodeGetContent(tmp)); */
-      	  /*     tmp = tmp->next; */
-      	  /*   } */
-      	}
+      if (parseTag((char *) cur->name, cur) == -1)
+	{
+	  xmlFree(cur);
+	  xmlFreeDoc(doc);
+	  return (EXIT_FAILURE);
+	}
       cur = cur->next;
     }
   xmlFree(cur);
   xmlFreeDoc(doc);
-  /* xmlFree(tmp); */
   return (EXIT_SUCCESS);
 }
--- parserXML/parserXml.h
+++ parserXML/parserXml.h
@@ -5,7 +5,7 @@
 ** Login   <mediav_j@epitech.net>
 ** 
 ** Started on  Fri May 22 15:07:06 2015 Jérémy Mediavilla
-** Last update Fri May 22 15:42:52 2015 Jérémy Mediavilla
+** Last update Tue May 26 12:26:07 2015 Jérémy Mediavilla
 */
 
 #ifndef		_PARSERXML_H_
@@ -15,6 +15,12 @@
 # include <libxml/parser.h>
 # include <libxml/tree.h>
 # include <string.h>
+
+# define PARAMETERS_TAG	"parameters"
+# define TEXTURE_TAG	"texture_pack"
+# define SOUND_TAG	"sound_pack"
+# define COMMON_TAG	"common"
+# define ENTITY_TAG	"entities"
 
 typedef struct	s_tag
 {
