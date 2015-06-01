377b35c3d3ea7ef11df3f0c154db04a2bed5f4b3
Florian Mari <mari_f@epitech.eu>
2015-06-01 12:50:57 +0200

    ajout modification vitesse du jeu, il faut faire fonction translate coté graph, merran?

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -5,7 +5,7 @@
 // Login   <jobertomeu@epitech.net>
 //
 // Started on  Tue May 19 13:09:39 2015 Joris Bertomeu
-// Last update Fri May 29 16:22:26 2015 mari_f
+// Last update Mon Jun  1 12:50:03 2015 mari_f
 //
 
 #include	<InputManager.hh>
@@ -34,21 +34,24 @@ void		InputManager::DetectKey(gdl::Input &input)
 
 void		InputManager::up()
 {
-  std::cout << "Up" << std::endl;
+  //translate(glm::vec3(0, 0, -1) * static_cast<float>(clock.getElapsed()));
 }
 
 void		InputManager::down()
 {
+  //translate(glm::vec3(0, 0, 1) * static_cast<float>(clock.getElapsed()));
   std::cout << "down" << std::endl;
 }
 
 void		InputManager::right()
 {
+  //translate(glm::vec3(-1, 0, 0) * static_cast<float>(clock.getElapsed()));
   std::cout << "right" << std::endl;
 }
 
 void		InputManager::left()
 {
+  //translate(glm::vec3(1, 0, 0) * static_cast<float>(clock.getElapsed()));
   std::cout << "left" << std::endl;
 }
 
