fc56c0b5a63fd02e7f2c088881c5dd1a3e61ce98
merran_g <geoffrey.merran@epitech.eu>
2015-06-12 23:52:20 +0200

    lol

--- src/Scenes/EscapeMenu.cpp
+++ src/Scenes/EscapeMenu.cpp
@@ -5,18 +5,17 @@
 // Login   <ades_n@epitech.eu>
 //
 // Started on  Fri Jun 12 11:12:51 2015 Nicolas Adès
-// Last update Fri Jun 12 02:32:36 2015 Nicolas Adès
+// Last update Fri Jun 12 23:51:13 2015 Geoffrey Merran
 //
 
 #include <EscapeMenu.hh>
 
 EscapeMenu::EscapeMenu(CameraManager &cm) : Scene(&cm)
 {
-  this->_btnNb = 0;
-  this->_buttons.push_back(new GameButton(glm::vec3(0, 300, 0), std::string("assets/textures/resume.tga")));
+  this->_buttons.push_back(new GameButton(glm::vec3(0, 300, 0), std::string("assets/textures/resume.tga"), RESUME));
   this->_buttons.front()->setCurrent(true);
-  this->_buttons.push_back(new GameButton(glm::vec3(0, 0, 0), std::string("assets/textures/save.tga")));
-  this->_buttons.push_back(new GameButton(glm::vec3(0, -300, 0), std::string("assets/textures/quit.tga")));
+  this->_buttons.push_back(new GameButton(glm::vec3(0, 0, 0), std::string("assets/textures/save.tga"), SAVE));
+  this->_buttons.push_back(new GameButton(glm::vec3(0, -300, 0), std::string("assets/textures/quit.tga"), QUIT));
   this->_eventHandler = new EscapeEvent();
   for (std::list<GameButton*>::iterator it = this->_buttons.begin(); it != this->_buttons.end(); it++)
     {
@@ -42,23 +41,42 @@ void					EscapeMenu::initialize()
   this->_cm->moveTo(glm::vec3(0, 0, 1000), glm::vec3(0, 0, 0));
 }
 
-void					EscapeMenu::moveCursor()
+void					EscapeMenu::moveCursorDown()
 {
   std::list<GameButton*>::iterator it = this->getCurrent();
   glm::vec3 pos;
-  
+
   (*it)->setCurrent(false);
   if (++it == this->_buttons.end())
     {
       this->_buttons.front()->setCurrent(true);
       pos = this->_buttons.front()->getPos();
-      this->_btnNb = 0;
     }
   else
     {
       (*it)->setCurrent(true);
       pos = (*it)->getPos();
-      this->_btnNb++;
+    }
+  pos.x = -500;
+  this->_cursor->setPos(pos);
+}
+
+void					EscapeMenu::moveCursorUp()
+{
+  std::list<GameButton*>::iterator it = this->getCurrent();
+  glm::vec3 pos;
+
+  (*it)->setCurrent(false);
+  if (it == this->_buttons.begin())
+    {
+      this->_buttons.back()->setCurrent(true);
+      pos = this->_buttons.back()->getPos();
+    }
+  else
+    {
+      it--;
+      (*it)->setCurrent(true);
+      pos = (*it)->getPos();
     }
   pos.x = -500;
   this->_cursor->setPos(pos);
@@ -86,20 +104,21 @@ int					EscapeMenu::getListSize() const
 
 void					EscapeMenu::selectButton(SceneManager *sm)
 {
-  switch (this->_btnNb)
+  std::string				nextScene = "";
+
+  switch ((*this->getCurrent())->getId())
     {
-    case 0:
-      if (!sm->setCurrentScene(std::string("gameScene")))
-	std::cerr << "Error while loading scene Game" << std::endl;      
+    case RESUME:
+      nextScene = "gameScene";
       break;
-    case 1:
-      std::cout << "Save GAME" << std::endl;
+    case SAVE:
       break;
-    case 2:
-      if (!sm->setCurrentScene(std::string("mainMenu")))
-	std::cerr << "Error while loading scene Game" << std::endl;      
+    case QUIT:
+      nextScene = "mainMenu";
       if (!sm->removeScene(std::string("gameScene")))
 	std::cerr << "Error while removing scene Game" << std::endl;
-	break;
+      break;
     }
+  if (!sm->setCurrentScene(nextScene))
+    std::cerr << "Error while loading scene: [" << nextScene << "]." << std::endl;
 }
