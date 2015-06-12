2372d534bc8527ed5425e7b5d7d8083441b89245
Florian Mari <mari_f@epitech.eu>
2015-06-12 15:47:28 +0200

    add thread class

---
+++ includes/ThreadC.hpp
@@ -0,0 +1,39 @@
+//
+// Thread.hpp for  in /home/mediav_j/rendu/cpp_plazza/includes
+// 
+// Made by Jérémy Mediavilla
+// Login   <mediav_j@epitech.net>
+// 
+// Started on  Tue Apr 21 16:24:49 2015 Jérémy Mediavilla
+// Last update Sat Apr 25 11:19:17 2015 Joris Bertomeu
+//
+
+#ifndef			_THREADC_HPP_
+# define		_THREADC_HPP_
+
+#include		<pthread.h>
+
+class			ThreadC
+{
+private:
+  pthread_t		_thread;
+
+public:
+  explicit		ThreadC() {
+    
+  };
+  virtual		~ThreadC() {
+    pthread_exit(&this->_thread);
+  };
+  int			threadJoin() {
+    return (pthread_join(this->_thread, NULL));
+  };
+  void			exec(void *(ptr_func)(void *)) {
+    pthread_create(&this->_thread, NULL, ptr_func, NULL);
+  }
+  void			exec(void *(ptr_func)(void *), void *arg) {
+    pthread_create(&this->_thread, NULL, ptr_func, arg);
+  }
+};
+
+#endif			// _THREAD_HPP_
